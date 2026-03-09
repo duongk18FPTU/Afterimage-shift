edeba36a3c0a26e4128ed6de8b0f6c19ff7ba491
duongk18FPTU <duong21102004@gmail.com>
2026-03-09 22:54:45 +0700

    AddArtAssets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+}
